1b918671c2aff947ba44010e0a697841f852a193
fanxuchao <fanxuchao_nj@163.com>
2017-06-03 22:03:25 +0800

    Spring	 in action Chapter 1.1.1

---
+++ src/SpringInAction/c1/BraveKnight.java
@@ -0,0 +1,14 @@
+package SpringInAction.c1;
+
+public class BraveKnight implements Knight{
+	public Quest quest;
+	
+	public BraveKnight(Quest quest){
+		this.quest = quest;
+	}
+	
+	public void embarkQuest(){
+		quest.embark();
+	}
+	
+}
---
+++ src/SpringInAction/c1/KillDragonQuest.java
@@ -0,0 +1,18 @@
+package SpringInAction.c1;
+
+import java.io.PrintStream;
+
+public class KillDragonQuest implements Quest {
+
+	PrintStream stream;
+	
+	public KillDragonQuest(PrintStream stream) {
+		this.stream = stream;
+	}
+	
+	@Override
+	public void embark() {
+		stream.println("AAAAA");
+	}
+
+}
